89301d12869515d02462f0695e3d216047def887
Jay Pal <contactjaypal@gmail.com>
2021-08-15 22:00:47 +0530

    custom custion rest api

--- MyApi/urls.py
+++ MyApi/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls'))
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('first-app/', include('firstApp.api.urls'))
 ]
