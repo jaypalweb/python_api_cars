5381223fbd7e06d82ff9bb3d76395c25262262b2
Jay Pal <contactjaypal@gmail.com>
2021-08-15 23:43:22 +0530

    getting the query param

--- firstApp/api/views.py
+++ firstApp/api/views.py
@@ -1,6 +1,8 @@
+from django.http import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view()
 def firstFunction(request):
+    print(request.query_params['id'])
     return Response({'message': "we received your request"})
