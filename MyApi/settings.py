e66f16906dd7d5642b2e6af7ec02b1c0cd82d6cc
Jay Pal <contactjaypal@gmail.com>
2021-08-16 17:24:41 +0530

    adding permissions rest api

--- MyApi/settings.py
+++ MyApi/settings.py
@@ -109,7 +109,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
--- firstApp/api/views.py
+++ firstApp/api/views.py
@@ -1,8 +1,10 @@
 from django.http import response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 @api_view()
+@permission_classes([AllowAny])
 def firstFunction(request):
-    print(request.query_params['id'])
+    # print(request.query_params['id'])
     return Response({'message': "we received your request"})
